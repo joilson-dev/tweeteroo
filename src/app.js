81758a560a60888f201884d024b2b89fa5a7f52c
Joilson Rodrigues <62257507+joilson-dev@users.noreply.github.com>
2024-10-02 22:26:08 -0300

    feat: add /tweets

--- src/app.js
+++ src/app.js
@@ -24,29 +24,56 @@ connectToDatabase();
 
 
 const userSchema = Joi.object({
-    username: Joi.string().required(),
+    userName: Joi.string().required(),
     avatar: Joi.string().uri().required(),
+});
+
+const tweetSchema = Joi.object({
+    userName: Joi.string().required(),
+    tweet: Joi.string().required(),
 });
 
 
 app.post('/sign-up', async (req, res) => {
-    const { username, avatar } = req.body;
+    const { userName, avatar } = req.body;
 
 
-    const { error } = userSchema.validate({ username, avatar }, { abortEarly: false });
+    const { error } = userSchema.validate({ userName, avatar }, { abortEarly: false });
 
     if (error) {
         return res.status(422).json({ errors: error.details.map(err => err.message) });
     }
 
     try {
-        await db.collection('users').insertOne({ username, avatar });
+        await db.collection('users').insertOne({ userName, avatar });
         return res.status(201).send('Usuário criado com sucesso');
     } catch (err) {
         return res.status(500).send('Erro ao salvar usuário');
     }
 });
 
+app.post('/tweets', async (req, res) => {
+    const { userName, tweet } = req.body
+    const { error } = tweetSchema.validate({ userName, tweet }, { abortEarly: false })
 
+    if (error) {
+        return res.status(422).json({ errors: error.details.map(err => err.message) });
+    }
+
+    try {
+
+        const user = await db.collection('users').findOne({ username: userName });
+
+        if (!user) {
+            return res.status(401).send('Usuário não cadastrado');
+        }
+
+        await db.collection('tweets').insertOne({ username: userName, tweet, avatar: user.avatar });
+
+        return res.status(201).send('Tweet criado com sucesso');
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+});
 
 app.listen(process.env.PORT, () => console.log(`Rodando na porta ${process.env.PORT}`))
